bfb54e01fb52af06d4060d9fb6607db7f7c8d2ca
Tony S <sanjayatony@gmail.com>
2020-12-04 17:24:43 +0700

    update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,5 @@
 <template>
   <footer class="max-w-xl text-sm font-code mt-24">
-    &copy; CMDKZ. Made with blood and sweat by
-    <a href="https://twitter.com/sanjayatony">Tony S.</a>
+    &copy; CMDKZ
   </footer>
 </template>
