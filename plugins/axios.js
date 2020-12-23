e9aecac0b7e034bdfc3817f26d964c6e0d6732b7
Tony S <sanjayatony@gmail.com>
2020-12-23 12:13:29 +0700

    remove unused code

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,4 @@
-export default function({ $axios, redirect }) {
-  // $axios.onRequest(config => {
-  //   config.headers.common["Authorization"] = "Bearer keyKlgrRtzhSKW3nC";
-  // });
+export default function({ $axios, redirect })
 
   $axios.onError(error => {
     if (error.response.status === 500) {
